2a4c03626a692dbb9ce869c2504d480aa8f064fb
Mohamed Arafet Khadraoui <146532740+itzragner@users.noreply.github.com>
2023-11-12 23:15:34 +0100

    tasks

--- tasks.php
+++ tasks.php
@@ -27,53 +27,61 @@ include 'config.php';
         <?php include 'navbar.php'; ?>
         <div class="d-flex flex-column" id="content-wrapper">
             <div id="content">
-            <?php include 'header.php'; ?>
-            <div class="container-fluid">
-                <?php
-                // Check if form is submitted by teacher
-                if(isset($_POST['submit_task'])){
-                    $task = $_POST['task'];
-                    // Insert task into database
-                    $sql = "INSERT INTO tasks (task, status) VALUES ('$task', 'uncompleted')";
-                    mysqli_query($conn, $sql);
-                }
+                <?php include 'header.php'; ?>
+                <div class="container-fluid">
+                            <div class="d-sm-flex justify-content-between align-items-center mb-2">
+                                <h3 class="text-dark mb-4 fw-bold">tasks</h3>
+                                <a class="btn btn-primary btn-sm d-none d-sm-inline-block " role="button" href="#"><i class="fas fa-download fa-sm text-white-50"></i> Export</a>
+                            </div>
+                            <div class="card shadow">
+                            <?php
+                            // Check if form is submitted by teacher
+                            if(isset($_POST['submit_task'])){
+                                $task = $_POST['task'];
+                                // Insert task into database
+                                $sql = "INSERT INTO tasks (task, status) VALUES ('$task', 'uncompleted')";
+                                mysqli_query($conn, $sql);
+                            }
 
-                // Check if form is submitted by student
-                if(isset($_POST['update_task'])){
-                    $task_id = $_POST['task_id'];
-                    $status = $_POST['status'];
-                    // Update task status in database
-                    $sql = "UPDATE tasks SET status='$status' WHERE id='$task_id'";
-                    mysqli_query($conn, $sql);
-                }
-                ?>
-                <?php if ($_SESSION['role'] != ('student' )):?>
-                <!-- Form for teacher to submit task -->
-                <form method="post" action="">
-                    <input type="text" name="task" placeholder="Enter task here">
-                    <input type="submit" name="submit_task" value="Submit Task">
-                </form>
-                <?php endif; ?>
+                            // Check if form is submitted by student
+                            if(isset($_POST['update_task'])){
+                                $task_id = $_POST['task_id'];
+                                $status = $_POST['status'];
+                                // Update task status in database
+                                $sql = "UPDATE tasks SET status='$status' WHERE id='$task_id'";
+                                mysqli_query($conn, $sql);
+                            }
+                            ?>
+                            <?php if ($_SESSION['role'] != ('student' )):?>
+                            <!-- Form for teacher to submit task -->
+                            <form method="post" action="">
+                                <input type="text" name="task" placeholder="Enter task here">
+                                <input type="submit" name="submit_task" value="Submit Task">
+                            </form>
+                            <?php endif; ?>
 
-                <?php if ($_SESSION['role'] != 'teacher'): ?>
-                <!-- Form for student to update task status -->
-                <form method="post" action="">
-                    <input type="hidden" name="task_id" value="1"> <!-- Replace 1 with actual task ID -->
-                    <select name="status">
-                        <option value="uncompleted">Uncompleted</option>
-                        <option value="ondoing">On Doing</option>
-                        <option value="completed">Completed</option>
-                    </select>
-                    <input type="submit" name="update_task" value="Update Task">
-                </form>
-                <?php endif; ?>
-            </div>
-            <footer class="bg-white sticky-footer">
-                <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright ©2023 designed By Med Arafet khadraoui</span></div>
+                            <?php if ($_SESSION['role'] != 'teacher'): ?>
+                            <!-- Form for student to update task status -->
+                            <form method="post" action="">
+                                <input type="hidden" name="task_id" value="1"> <!-- Replace 1 with actual task ID -->
+                                <select name="status">
+                                    <option value="uncompleted">Uncompleted</option>
+                                    <option value="ondoing">On Doing</option>
+                                    <option value="completed">Completed</option>
+                                </select>
+                                <input type="submit" name="update_task" value="Update Task">
+                            </form>
+                            <?php endif; ?>
+                        </div>
+                        <footer class="bg-white sticky-footer">
+                            <div class="container my-auto">
+                                <div class="text-center my-auto copyright"><span>Copyright ©2023 designed By Med Arafet khadraoui</span></div>
+                            </div>
+                        </footer>
                 </div>
-            </footer>
-        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+            </div>
+            <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        </div>
     </div>
     
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
